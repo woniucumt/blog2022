141cec99c8f8fa0cd2acab80ff29c39b5f4a2109
woniucumt <535633437@qq.com>
2022-05-12 20:42:21 +0800

    all_host

--- blog2022/blog2022/settings.py
+++ blog2022/blog2022/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z(81x)e4hewbt)=-q^fc^$7@z^wro9ab5#bbxv64xoqb6$mlxx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*',]
 
